98d70fc75a7e63679e2cbc7a3468c3e51089f9c5
Lohrke <heiko.lohrke@ubirch.com>
2020-06-05 19:45:53 +0200

    [wip] Add deepsleep/lib demo/debug code to main.py

--- micropython/main.py
+++ micropython/main.py
@@ -5,11 +5,54 @@ import os
 import sys
 import ubinascii as binascii
 from network import WLAN, LTE
+from machine import RTC
 
 from helpers import *
 from ubirch import SimProtocol
 
+def send_at_cmd_pretty(cmd):
+    response = lte.send_at_cmd(cmd).split('\r\n')
+    for line in response:
+        print(line)
+
+def establish_connection():
+    #establish LTE connection
+    tries = 0
+    if not lte.isattached():
+        print("Attaching...")
+        set_led(LED_ORANGE)
+        lte.attach(apn=cfg.get("apn"))    
+        while not lte.isattached():
+            tries+=1
+            if tries > 20*4:
+                print("\nResetting...")
+                lte.reset()
+                machine.reset()
+            time.sleep(0.25)
+            sys.stdout.write(".")
+    else:
+        print("Already attached")
+
+    tries=0
+    if not lte.isconnected():
+        print("Connecting...")
+        set_led(LED_YELLOW)
+        lte.connect()    
+        while not lte.isconnected():
+            tries+=1
+            if tries > 5*4:
+                print("\nResetting...")
+                lte.reset()
+                machine.reset()
+            time.sleep(0.25)
+            sys.stdout.write(".")
+    else:
+        print("Already connected")
+        
+    set_led(LED_GREEN)
+
 print("\n- - - UBIRCH protocol (SIM) - - -\n")
+set_led(LED_RED)
 
 # load some necessary config (request API keys from ubirch)
 with open("config.json") as f:
@@ -23,46 +66,14 @@ device_name = "ukey"
 cert_id = "ucrt"
 
 lte = LTE()
-lte.reset()
 
-if 'wifi' in cfg:
-    nb_iot_connection = False
-    func_lvl = 4  # disable modem transmit and receive RF circuits
-else:
-    nb_iot_connection = True
-    func_lvl = 1  # full modem functionality
+if machine.reset_cause() != machine.DEEPSLEEP_RESET:
+    #if we are not coming from deepsleep, the modem is probably in a strange state -> reset
+    print("Not coming from sleep, resetting modem to be safe...")
+    lte.reset()
+    lte.init()
 
-# set up modem
-try:
-    set_modem_func_lvl(lte, func_lvl)
-except Exception as e:
-    set_led(LED_PURPLE)
-    sys.print_exception(e)
-    reset()
-
-# set up network connection
-if nb_iot_connection:
-    # check Network Coverage for UE device (i.e LTE modem)
-    if not lte.ue_coverage():
-        print("!! There seems to be no Network Coverage !! Try to attach and connect anyway ...")
-
-    # initialize LTE and connect to LTE network
-    try:
-        lte_setup(lte, nb_iot_connection, cfg.get("apn"))
-    except Exception as e:
-        set_led(LED_PURPLE)
-        sys.print_exception(e)
-        lte_shutdown(lte, reset_modem=True)
-        reset()
-else:
-    # initialize wifi connection
-    wlan = WLAN(mode=WLAN.STA)
-    try:
-        wifi_connect(wlan, cfg["wifi"]["ssid"], cfg["wifi"]["pass"])
-    except Exception as e:
-        set_led(LED_PURPLE)
-        sys.print_exception(e)
-        reset()
+establish_connection()
 
 # set time
 try:
@@ -70,17 +81,36 @@ try:
 except Exception as e:
     set_led(LED_PURPLE)
     sys.print_exception(e)
-    lte_shutdown(lte)
+    #lte_shutdown(lte)
     reset()
 
+print("Suspending data session for AT commands")
+lte.pppsuspend()
+
+
+print("Open new APDU channel")
+send_at_cmd_pretty('AT+CSIM=10,"0070000001"')
+
+#print("Send AT Commands")
+# print('Select Applet')
+# send_at_cmd_pretty('AT+CSIM=42,"01A4040010D2760001180002FF34108389C0028B02"')
+# print("Authenticate")
+# send_at_cmd_pretty('AT+CSIM=18,"012000000432343934"')
+# print("Get random data")
+# send_at_cmd_pretty('AT+CSIM=10,"81B9002000"')
+# print("Get first SS entry")
+# send_at_cmd_pretty('AT+CSIM=10,"81A5010000"')
+
+
+print("Initializing ubirch sim protocol")
 # initialize the ubirch protocol interface
 ubirch = None
 try:
-    ubirch = SimProtocol(lte=lte, at_debug=cfg.get("debug", False))
+    ubirch = SimProtocol(lte=lte, at_debug=False)
 except Exception as e:
     set_led(LED_RED)
     sys.print_exception(e)
-    lte_shutdown(lte)
+    #lte_shutdown(lte)
     reset()
 
 # get IMSI from SIM
@@ -114,99 +144,77 @@ if not ubirch.sim_auth(pin):
 device_uuid = ubirch.get_uuid(device_name)
 print("UUID: {}\n".format(device_uuid))
 
-# try to get X.509 certificate from SIM
-csr = b''
+# # try to get X.509 certificate from SIM
+# csr = b''
+# try:
+#     csr = ubirch.get_certificate(cert_id)
+#     print("X.509 certificate [hex]   : " + binascii.hexlify(csr).decode())
+#     print("X.509 certificate [base64]: " + binascii.b2a_base64(csr).decode())
+# except Exception as e:
+#     print("getting X.509 certificate from SIM failed: {}\n".format(e))
+
+#     # create a self-signed certificate for the public key
+#     print("-- creating self-signed certificate for identity {}".format(device_uuid))
+#     csr = get_certificate(device_name, device_uuid, ubirch)
+#     print("cert: {}\n".format(csr.decode()))
+
+#     # register public key at ubirch key service
+#     try:
+#         print("resp: {}\n".format(register_key(KEY_SERVER, cfg["password"], csr).decode()))
+#     except Exception as e:
+#         set_led(LED_ORANGE)
+#         sys.print_exception(e)
+#         lte_shutdown(lte)
+#         reset()
+
+# get data
+payload_data = binascii.hexlify(crypto.getrandbits(32))
+
+# create message with timestamp, UUID and data to ensure unique hash
+message = '{{"ts":{},"id":"{}","data":"{}"}}'.format(
+    time.time(),
+    device_uuid,
+    binascii.b2a_base64(payload_data).decode().rstrip('\n'))
+print("\nmessage: {}\n".format(message))
+
+# generate UPP with the message hash using the automatic hashing functionality of the SIM card
 try:
-    csr = ubirch.get_certificate(cert_id)
-    print("X.509 certificate [hex]   : " + binascii.hexlify(csr).decode())
-    print("X.509 certificate [base64]: " + binascii.b2a_base64(csr).decode())
+    upp = ubirch.message_chained(device_name, message.encode(), hash_before_sign=True)
 except Exception as e:
-    print("getting X.509 certificate from SIM failed: {}\n".format(e))
+    set_led(LED_RED)
+    sys.print_exception(e)
+    time.sleep(3)
 
-    # create a self-signed certificate for the public key
-    print("-- creating self-signed certificate for identity {}".format(device_uuid))
-    csr = get_certificate(device_name, device_uuid, ubirch)
-    print("cert: {}\n".format(csr.decode()))
+print("UPP (msgpack): {}\n".format(binascii.hexlify(upp).decode()))
+print("hash (SHA256): {}".format(binascii.b2a_base64(get_upp_payload(upp)).decode()))
 
-    # register public key at ubirch key service
-    try:
-        print("resp: {}\n".format(register_key(KEY_SERVER, cfg["password"], csr).decode()))
-    except Exception as e:
-        set_led(LED_ORANGE)
-        sys.print_exception(e)
-        lte_shutdown(lte)
-        reset()
+print("Closing APDU channel")
+send_at_cmd_pretty('AT+CSIM=10,"0070800100"')
 
-interval = 60
-print("-- starting loop (interval = {} sec)\n".format(interval))
-while True:
-    start_time = wake_up()  # start timer
+print("Done with AT commands, resuming data session")
+lte.pppresume()
 
-    # reinitialize LTE and reconnect to LTE network
-    try:
-        lte_setup(lte, nb_iot_connection, cfg.get("apn"))
-        ubirch.reinit(pin)
-    except Exception as e:
-        set_led(LED_PURPLE)
-        sys.print_exception(e)
-        lte_shutdown(lte)
-        reset()
 
-    # get data
-    payload_data = binascii.hexlify(crypto.getrandbits(32))
 
-    # create message with timestamp, UUID and data to ensure unique hash
-    message = '{{"ts":{},"id":"{}","data":"{}"}}'.format(
-        start_time,
-        device_uuid,
-        binascii.b2a_base64(payload_data).decode().rstrip('\n'))
-    print("\nmessage: {}\n".format(message))
+# # # # # # # # # # # # # # # # # # #
+# send message to your backend here #
+# # # # # # # # # # # # # # # # # # #
 
-    # generate UPP with the message hash using the automatic hashing functionality of the SIM card
-    try:
-        upp = ubirch.message_chained(device_name, message.encode(), hash_before_sign=True)
-    except Exception as e:
-        set_led(LED_RED)
-        sys.print_exception(e)
-        time.sleep(3)
-        continue
+# send UPP to ubirch backend
+try:
+    establish_connection()
+    print("Sending UPP")
+    post(UPP_SERVER, device_uuid, cfg["password"], upp)
+except Exception as e:
+    set_led(LED_ORANGE)
+    sys.print_exception(e)
 
-    print("UPP (msgpack): {}\n".format(binascii.hexlify(upp).decode()))
-    print("hash (SHA256): {}".format(binascii.b2a_base64(get_upp_payload(upp)).decode()))
 
-    # make sure device is still connected before sending data
-    try:
-        if nb_iot_connection:
-            # check NB-IOT connection
-            if not lte.isconnected():
-                nb_iot_connect(lte)
-        else:
-            # check WIFI connection
-            wlan = WLAN(mode=WLAN.STA)
-            if not wlan.isconnected():
-                wifi_connect(wlan, cfg["wifi"]["ssid"], cfg["wifi"]["pass"])
-    except Exception as e:
-        set_led(LED_PURPLE)
-        sys.print_exception(e)
-        lte_shutdown(lte)
-        reset()
+print("Disconnect LTE")
+lte.disconnect()
 
-    # # # # # # # # # # # # # # # # # # #
-    # send message to your backend here #
-    # # # # # # # # # # # # # # # # # # #
+print("Deinit LTE")
+lte.deinit(detach=False)
 
-    # send UPP to ubirch backend
-    try:
-        post(UPP_SERVER, device_uuid, cfg["password"], upp)
-    except Exception as e:
-        set_led(LED_ORANGE)
-        sys.print_exception(e)
-        time.sleep(3)
-        continue
-
-    # if lte.isconnected():
-    #     print(">> disconnecting LTE")
-    #     lte.disconnect()
-    lte_shutdown(lte, detach=False)
-
-    sleep_until_next_interval(start_time, interval)
+print("Going to sleep")
+machine.deepsleep(10000)
